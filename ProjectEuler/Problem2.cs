a18fc3eddbed00f519b68666da7331836727305b
Nick Anderson <nanderson@reliaslearning.com>
2017-11-17 07:59:51 -0500

    Problem 2 files

---
+++ ProjectEuler/Problem2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem2 : IRunnable
+    {
+        public void Run()
+        {
+            var first = 1;
+            var second = 2;
+            var sum = 0;
+            while (second <= 4000000)
+            {
+                if (second % 2 == 0)
+                {
+                    sum += second;
+                }
+                var temp = second;
+                second = first + second;
+                first = temp;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
