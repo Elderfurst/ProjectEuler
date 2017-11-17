51b7d2df1cfd08d8956d87c8685937848b083e7e
Nick Anderson <nanderson@reliaslearning.com>
2017-11-17 07:39:50 -0500

    Initial commit

---
+++ ProjectEuler/Problem1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem1 : IRunnable
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
