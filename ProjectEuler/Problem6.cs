ea07955b0a51859f3aa6087106ae61bfbbeb0490
Nick Anderson <nanderson@reliaslearning.com>
2017-11-21 13:40:36 -0500

    Problem 6 solution

---
+++ ProjectEuler/Problem6.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem6 : IRunnable
+    {
+        public void Run()
+        {
+            const int num = 100;
+            Console.WriteLine(SquareOfSums(num) - SumOfSquares(num));
+        }
+
+        private long SumOfSquares(int maxNum)
+        {
+            var sumOfSquares = 0;
+            for (var i = 1; i <= maxNum; i++)
+            {
+                sumOfSquares += (i * i);
+            }
+            return sumOfSquares;
+        }
+
+        private long SquareOfSums(int maxNum)
+        {
+            var squareOfSums = 0;
+            for (var i = 1; i <= maxNum; i++)
+            {
+                squareOfSums += i;
+            }
+            return squareOfSums * squareOfSums;
+        }
+    }
+}
