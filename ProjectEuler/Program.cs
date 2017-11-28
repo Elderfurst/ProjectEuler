4c141211ac5fffcd6d8cfcd7b1023b7a53b0c689
Nick Anderson <nanderson@reliaslearning.com>
2017-11-28 16:27:06 -0500

    Problem 13 solution

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            var run = new Problem12();
+            var run = new Problem13();
             run.Run();
         }
     }
