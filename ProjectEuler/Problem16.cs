0d2d0b4a6540286bde32396d083569cc1579288d
Nick Anderson <nanderson@reliaslearning.com>
2017-12-04 15:48:01 -0500

    Problem 16 soution

---
+++ ProjectEuler/Problem16.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Problem16
+    {
+        public void Run()
+        {
+            var result = (BigInteger) Math.Pow(2, 1000);
+            //Convert the resulting number into a character array, parse each character into an int, and then add them all together
+            Console.WriteLine(result.ToString().ToCharArray().Select(x => int.Parse(x.ToString())).Sum());
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            var run = new Problem15();
+            var run = new Problem16();
             run.Run();
         }
     }
