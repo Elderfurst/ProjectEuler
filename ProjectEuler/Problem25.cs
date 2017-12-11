3554aa6293d28915a1a16b2cf99ea6523fcc9119
Nick Anderson <nanderson@reliaslearning.com>
2017-12-11 15:40:10 -0500

    Problem 25 solution

---
+++ ProjectEuler/Problem25.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Problem25
+    {
+        public void Run()
+        {
+            BigInteger first = 1;
+            BigInteger second = 1;
+            BigInteger counter = 2;
+            while (second.ToString().Length < 1000)
+            {
+                var temp = second;
+                second = first + second;
+                first = temp;
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            var run = new Problem24();
+            var run = new Problem25();
             run.Run();
         }
     }
