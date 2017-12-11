51a1e40ceeb6465588d10313c215e34e243a2b1d
Nick Anderson <nanderson@reliaslearning.com>
2017-12-11 10:25:14 -0500

    Problem 22 solution

---
+++ ProjectEuler/Problem22.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem22
+    {
+        private readonly string[] _names = File.ReadAllText(@"Input/Problem22.txt").Split(',')
+            .Select(x => x.Replace("\"", "")).OrderBy(x => x).ToArray();
+        public void Run()
+        {
+            Console.WriteLine(_names.Sum(name => name.Sum(letter => letter < 97 ? letter - 64 : letter - 96) * (Array.IndexOf(_names, name) + 1)));
+        }
+    }
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            var run = new Problem21();
+            var run = new Problem22();
             run.Run();
         }
     }
